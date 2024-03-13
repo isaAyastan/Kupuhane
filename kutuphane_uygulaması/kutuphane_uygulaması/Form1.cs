d05a7e6c92a61ecdd605509ba9154c008aaa76a5
isaAyastan <163300225+isaAyastan@users.noreply.github.com>
2024-03-13 14:54:38 +0300

    Add files via upload

---
+++ kutuphane_uygulaması/kutuphane_uygulaması/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace kutuphane_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbCommand komut;
+        OleDbConnection baglanti;
+        OleDbDataAdapter da;
+      
+        void listele()
+        {
+            baglanti = new OleDbConnection("Provider = Microsoft.Jet.OleDb.4.0;Data Source =  kutuphane.mdb");
+            baglanti.Open();
+            da = new OleDbDataAdapter("Select * From kitaplar",baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO kitaplar (adi,yazar,tur,raf,sayfa_sayisi) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "')";
+            OleDbCommand komut = new OleDbCommand(sorgu, baglanti);
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+            listele();
+
+            
+        }     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                string sorgu = " DELETE FROM kitaplar WHERE kimlik = " + textBox6.Text + "";
+                komut = new OleDbCommand(sorgu, baglanti);
+                baglanti.Open();
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+
+                listele();
+            
+            
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox6.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+           textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+             
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+            textBox4.Text = " ";
+            textBox5.Text = " ";
+            textBox6.Text = " ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE kitaplar SET adi = '"  +textBox1.Text  +"', yazar='"+textBox2.Text+"', tur='"+textBox3.Text+"',raf='"+textBox4.Text+"',sayfa_sayisi='"+textBox5.Text+"' WHERE Kimlik = " + textBox6.Text;
+            komut = new OleDbCommand(sorgu, baglanti);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+            listele();
+        }
+    }
+}
---
+++ kutuphane_uygulaması/kutuphane_uygulaması/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace kutuphane_uygulaması
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        OleDbCommand komut;
+        OleDbConnection baglanti;
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullanıcı = "SELECT * FROM Veri_Girisi";
+
+            baglanti = new OleDbConnection("Provider = Microsoft.Jet.OleDb.4.0;Data Source =  kutuphane.mdb");
+
+            komut = new OleDbCommand(kullanıcı, baglanti);
+            baglanti.Open();
+
+            OleDbDataReader oku = komut.ExecuteReader();
+            oku.Read();
+
+            if (oku.GetValue(1).ToString() == textBox1.Text && oku.GetValue(2).ToString() == textBox2.Text)
+            {
+                Form1 burak = new Form1();
+                burak.Show();
+            }
+            else
+            {
+                MessageBox.Show("Yanlis");
+            }
+            baglanti.Close();
+
+        }
+    }
+}
